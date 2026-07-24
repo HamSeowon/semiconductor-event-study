bbc2fe3a81cbec51a738094dffbfd1377f855c19
Seowon Ham <seowonham@fe:9f:10:cd:a0:10.home>
2026-07-24 02:50:42 -0400

    Writing a script to collect stock price and KOSPI

--- src/data_collected.py
+++ src/data_collected.py
@@ -1,35 +1,26 @@
 import FinanceDataReader as fdr
-from pykrx import stock
-import pandas as pd
-
 
 START_DATE = "2026-01-01"
 END_DATE = "2026-07-22"
 
-STOCK_CODE = "000660" #sk hynix code
-
+#sk hynix stock price
 def collect_price_data():
-    """SK하이닉스 일별 주가(OHLCV) 수집"""
-    print("SK하이닉스 주가 데이터 수집 중...")
-    df = fdr.DataReader(STOCK_CODE, START_DATE, END_DATE)
+    df = fdr.DataReader("000660", START_DATE, END_DATE)
     df.to_csv("data/raw/sk_hynix_price.csv")
-    print(f"완료: {len(df)}행 저장됨 -> data/raw/sk_hynix_price.csv")
     return df
 
+#KOSPI
 def collect_kospi_data():
-    """코스피 지수 수집 (비교 벤치마크)"""
-    print("코스피 지수 데이터 수집 중...")
     df = fdr.DataReader("KS11", START_DATE, END_DATE)
     df.to_csv("data/raw/kospi.csv")
-    print(f"완료: {len(df)}행 저장됨 -> data/raw/kospi.csv")
     return df
 
 if __name__ == "__main__":
     price_df = collect_price_data()
     kospi_df = collect_kospi_data()
 
-    print("\n=== 수집 결과 미리보기 ===")
-    print("\n[주가]")
+    print("\ Preview")
+    print("\n Stock Price")
     print(price_df.head())
-    print("\n[코스피]")
+    print("\n KOSPI")
     print(kospi_df.head())
