3eaa2589b6d0d3938a2848e040283d01fa3b1a07
Seowon Ham <seowonham@seowons-laptop.home>
2026-07-27 19:10:18 -0400

    data_collected.py Generalized to support multiple tickers

--- src/data_collected.py
+++ src/data_collected.py
@@ -1,26 +1,46 @@
 import FinanceDataReader as fdr
+import pandas as pd
 
 START_DATE = "2026-01-01"
-END_DATE = "2026-07-22"
+END_DATE = "2026-07-26"
 
-#sk hynix stock price
-def collect_price_data():
-    df = fdr.DataReader("000660", START_DATE, END_DATE)
-    df.to_csv("data/raw/sk_hynix_price.csv")
+#add/delete tickers
+TICKERS = {
+    "000660" : "sk_hynix",
+    "005930" : "samsung_electronics"
+}
+
+#stock prices @param stock ticker
+def collect_price_data(ticker):
+    df = fdr.DataReader(ticker, START_DATE, END_DATE)
+    df["ticker"] = ticker
+    df = df.reset_index()
     return df
+
+def collect_all_prices():
+    all_data = []
+    for ticker in TICKERS:
+        df = collect_price_data(ticker)
+        all_data.append(df)
+    combined = pd.concat(all_data,ignore_index = True)
+    combined.to_csv("data/raw/multi_stock_price.csv", index = False)
+    return combined
 
 #KOSPI
 def collect_kospi_data():
     df = fdr.DataReader("KS11", START_DATE, END_DATE)
-    df.to_csv("data/raw/kospi.csv")
+    df["ticker"] = "KOSPI"
+    df = df.reset_index()
+    df.to_csv("data/raw/kospi.csv",index=False)
     return df
 
 if __name__ == "__main__":
-    price_df = collect_price_data()
+    price_df = collect_all_prices()
     kospi_df = collect_kospi_data()
 
-    print("\ Preview")
+    print("\n Preview")
     print("\n Stock Price")
     print(price_df.head())
     print("\n KOSPI")
     print(kospi_df.head())
+    print(price_df.groupby("ticker").size())
