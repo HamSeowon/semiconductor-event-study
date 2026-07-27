502227a7a3ee6333a16e2e0760088c69cbb84f23
Seowon Ham <seowonham@seowons-laptop.home>
2026-07-27 19:35:34 -0400

    build_db.py Updated to support multiple tickers

--- src/build_db.py
+++ src/build_db.py
@@ -1,7 +1,7 @@
 import sqlite3
 import pandas as pd
 
-DB_PATH = "data/sk_hynix.db"
+DB_PATH = "data/semiconductor.db"
 
 def create_tables(connection):
     with open("sql/schema.sql", "r") as f:
@@ -10,20 +10,19 @@ def create_tables(connection):
 
 
 def load_price_data(connection):
-    df = pd.read_csv("data/processed/sk_hynix_price_clean.csv")
-    df.columns = df.columns.str.lower()  
-    df.rename(columns={"date": "date"}, inplace=True)
+    df = pd.read_csv("data/processed/multi_stock_price_clean.csv",dtype={"ticker":str})
+    # df.columns = df.columns.str.lower()  
+    # df.rename(columns={"date": "date"}, inplace=True)
     df.to_sql("daily_price", connection, if_exists="replace", index=False)
 
 def load_kospi_data(connection):
-    df = pd.read_csv("data/processed/kospi_clean.csv")
+    df = pd.read_csv("data/processed/kospi_clean.csv",dtype={"ticker":str})
     df.columns = df.columns.str.lower()
     df = df[["date", "close", "daily_return"]].copy()
-    df["ticker"] = "KOSPI"
     df.to_sql("benchmark_price", connection, if_exists="replace", index=False)
 
 def load_events_data(connection):
-    df = pd.read_csv("data/raw/sk_hynix_event_log.csv")
+    df = pd.read_csv("data/raw/event_log.csv",dtype={"ticker":str})
     df.to_sql("events", connection, if_exists="replace", index=False)
 
 if __name__ == "__main__":
